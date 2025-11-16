d72e9ec7ff6d2356aa60391bbdb1b471c68f3c3c
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2025-11-15 22:06:59 -0500

    build crd functions in view

--- routes/todo.js
+++ routes/todo.js
@@ -3,3 +3,8 @@ const router = express.Router();
 
 const { create, read, removeTodo } = require('../controller')
 
+router.post('/todo/create', create);
+router.get('/todos', read);
+router.delete('/todo/:id', removeTodo);
+
+module.exports = router;
